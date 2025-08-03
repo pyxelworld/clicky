b51e221095598cf1b1f4e9daa663f28799f9d87b
Pyxel <134378477+pyxelworld@users.noreply.github.com>
2025-08-03 18:26:17 +0000

    8000

--- sixsec/app.py
+++ sixsec/app.py
@@ -1919,4 +1919,4 @@ if __name__ == '__main__':
     with app.app_context():
         if not os.path.exists(app.config['UPLOAD_FOLDER']): os.makedirs(app.config['UPLOAD_FOLDER'])
         db.create_all()
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=8000)
